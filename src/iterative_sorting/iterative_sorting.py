20b7675b87bd874f1e1b0f4febf272b22308030e
Josh Solis <joshsolis@Joshs-MBP.fios-router.home>
2019-06-03 17:54:40 -0400

    day 1 work

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -7,16 +7,12 @@ def selection_sort( arr ):
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc) .
         for j in range(1, len(arr)-1):
-            second_cur_index = j
-            next_smallest = second_cur_index
+            if arr[smallest_index] > arr[j]:
+                arr[smallest_index] = arr[j]
 
 
 
         # TO-DO: swap
-            if arr[smallest_index] > arr[next_smallest]:
-                x = arr[smallest_index] 
-                arr[smallest_index] = arr[next_smallest]
-                arr[next_smallest] = x
 
 
     return arr
